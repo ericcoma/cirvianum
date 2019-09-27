8232fe72141c49a2be821aecb2aa703b7f47e569
Eric Coma <ericxddd5@gmail.com>
2019-09-27 16:12:34 +0200

    creació codi suma

---
+++ suma.java
@@ -0,0 +1,9 @@
+int numero1 = 0;
+int numero2 = 0;
+
+System.out.pritnln("Introduce el primer numero:");
+numero1 = reader.nextInt();
+
+System.out.pritnln("Introduce el segundo numero:");
+numero2 = reader.nextInt();
+
