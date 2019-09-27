0cb769cd87413b4934d53d4acdf722f03a7ed61b
Sergi Sabata <seibat21@gmail.com>
2019-09-27 18:02:48 +0200

    Canvis   suma.java per sergi
    He canviat unes quantes linies de codi a suma.java

--- suma.java
+++ suma.java
@@ -1,9 +1,11 @@
-int number1 = 0;
-int number2 = 0;
+No funciona res perque t'ho he comentat
+-Sergi
+//int number1 = 0;
+//int number2 = 0;
 
-System.out.pritnln("write first number:");
-number1 = reader.nextInt();
+//System.out.pritnln("write first number:");
+//number1 = reader.nextInt();
 
-System.out.pritnln("write second number:");
-number2 = reader.nextInt();
+//System.out.pritnln("write second number:");
+//number2 = reader.nextInt();
 
